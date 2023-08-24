63cbdf8e8e1573b88b6f9374fcca7a9f9f02c42b
Lucas Souza <lucasrsouzasilva@gmail.com>
2023-08-24 17:19:22 -0300

    aula 8

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,9 +22,11 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
         'email',
         'password',
+        'advisor_id'
     ];
 
     /**
@@ -62,15 +64,4 @@ class User extends Authenticatable
         return $this->belongsTo(Order::class);
     }
 
-    #[SearchUsingPrefix(['id', 'email'])]
-    #[SearchUsingFullText(['first_name', 'last_name'])]
-    public function toSearchableArray()
-    {
-        return [
-            'id' => $this->id,
-            'email' => $this->email,
-            'first_name' => $this->first_name,
-            'last_name' => $this->last_name
-        ];
-    }
 }
